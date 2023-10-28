f1ecda4355174c8ab851df14e4baccc28a0550f1
mohankumar01012005 <kumar.mohan@kalvium.community>
2023-10-29 00:33:23 +0530

    day10

---
+++ day-10/script.js
@@ -0,0 +1,22 @@
+  //9. Math.random()
+
+    // console.log(Math.random()*10);              // this will give you random numbers with decimal values
+    // console.log(Math.floor(Math.random()*10));  // this will give you random numbers (dont gives you 10 because )
+
+   
+   //10.Math.trunc()
+
+      // console.log(Math.trunc(44.3));
+      // console.log(Math.trunc(44.9));        // it will return you the value before decimal point
+
+
+  // practice time:
+
+   // ==>  if argument is positive number , Math.trunk() method is equalant to Math.floor(), otherwise 
+  // Math.trunc() is equalant to Math.ceil() method
+
+        // console.log(Math.trunc(-6.7));
+        // console.log(Math.trunc(6.7));
+
+        // console.log(Math.trunc(-6.2));
+        // console.log(Math.trunc(6.2));
--- day-9/script222.js
+++ day-9/script222.js
@@ -4,18 +4,18 @@
   //   document.getElementById("demo").innerHTML = d;
   // }
 
-(function(){
-  setInterval(()=>{
-    var d  = new Date().toLocaleTimeString();
-    document.getElementById("demo").innerHTML = d;
+// (function(){
+//   setInterval(()=>{
+//     var d  = new Date().toLocaleTimeString();
+//     document.getElementById("demo").innerHTML = d;
 
-  },1000)
-})();
+//   },1000)
+// })();
 
-var d  =  new Date().toLocaleTimeString(); 
-document.getElementById("demo").innerHTML = d;
+// var d  =  new Date().toLocaleTimeString(); 
+// document.getElementById("demo").innerHTML = d;
 
 
-var t  = new Date().toLocaleDateString(); 
-document.getElementById("time").innerHTML = t;
+// var t  = new Date().toLocaleDateString(); 
+// document.getElementById("time").innerHTML = t;
 

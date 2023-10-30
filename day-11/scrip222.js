ba92fb111bd9fb79488ebcbbc5167ea9c088e17f
mohankumar01012005 <kumar.mohan@kalvium.community>
2023-10-31 00:10:23 +0530

    day-11

---
+++ day-11/scrip222.js
@@ -0,0 +1,20 @@
+// function goBack(){
+//   window.history.back();
+// }
+///////////////////////////////////////////////////////////
+// var secondChild = document.querySelector('.child-two');
+// secondChild.style.color="blue"
+/////////////////////////////////////////////////////////
+// var btn = document.getElementById("button");
+// button.onclick="changeContent()"
+
+// const changeContent = ()=>{
+//   document.getElementById('heading').innerHTML="you text has been changed"
+                  
+// }
+                    // OR
+const changeContent = ()=>{
+  var change= document.getElementById('heading')
+  change.innerHTML="you text has been changed"
+ 
+ }

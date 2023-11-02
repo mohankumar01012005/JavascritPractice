d5319077275d42bd1f326609600afda77ea18147
mohankumar01012005 <kumar.mohan@kalvium.community>
2023-11-02 23:36:23 +0530

    day14

---
+++ day-14/script.js
@@ -0,0 +1,39 @@
+// // <!-- learnt about the figma design -->
+// function Person(name, age) {
+//   this.name = name;
+//   this.age = age;
+//   this.sayHello = function() {
+//     console.log(`Hello, my name is ${this.name}.`);
+//   };
+// }
+
+// const person1 = new Person('Alice', 30);
+// console.log(person1);
+
+
+// const calculator = {
+//   add: function(a, b) {
+//     return a + b;
+//   },
+// };
+
+
+// function sum(...numbers) {
+//   return numbers.reduce((acc, num) => acc + num, 0);
+// }
+
+// const result = sum(1, 2, 3, 4, 5);
+
+
+// function* generateNumbers() {
+//   yield 1;
+//   yield 2;
+//   yield 3;
+// }
+
+
+
+// const gen = generateNumbers();
+// console.log(gen.next().value); // 1
+// console.log(gen.next().value); // 2
+// console.log(gen.next().value); // 3
